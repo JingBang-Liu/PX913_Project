4d96d6080c35d1f0bad84af689011b977c359bb9
Aravinthen Rajkumar <arre.rajkumar@warwick.ac.uk>
2019-12-03 16:24:23 +0000

    Bizarre bug encountered. Do not use!

--- verlet_integrator.f90
+++ verlet_integrator.f90
@@ -9,12 +9,30 @@ MODULE verlet_integrator
   TYPE kinematics
      ! global type
      ! This stores all of the kinematic data of a run.
-     REAL(KIND=REAL64), DIMENSION(:,:), ALLOCATABLE :: pos_history ! position history
-     REAL(KIND=REAL64), DIMENSION(:,:), ALLOCATABLE :: vel_history ! velocity history
-     REAL(KIND=REAL64), DIMENSION(:,:), ALLOCATABLE :: acc_history ! acceleration history          
+     REAL(KIND=REAL64), ALLOCATABLE :: pos_history(:,:) ! position history
+     REAL(KIND=REAL64), ALLOCATABLE :: vel_history(:,:) ! velocity history
+     REAL(KIND=REAL64), ALLOCATABLE :: acc_history(:,:) ! acceleration history
   END type kinematics
   
 CONTAINS
+
+  SUBROUTINE fillType(data_list, T)
+    ! fills the kinematics data arrays before use.
+    TYPE(kinematics) :: data_list
+    INTEGER :: T
+
+    ALLOCATE(data_list%pos_history(2,T))
+    ALLOCATE(data_list%vel_history(2,T))
+    ALLOCATE(data_list%acc_history(2,T))        
+  END SUBROUTINE fillType
+
+  SUBROUTINE emptyType(data_list)
+    ! empty the kinematics data arrays.
+    TYPE(kinematics) :: data_list
+    DEALLOCATE(data_list%pos_history)
+    DEALLOCATE(data_list%vel_history)
+    DEALLOCATE(data_list%acc_history)
+  END SUBROUTINE emptyType
 
   FUNCTION pos2cell(position, delta)
     ! delta refers to the size of the spatial discretization step.
@@ -26,7 +44,7 @@ CONTAINS
     pos2cell = FLOOR((position - 1.0_REAL64)/delta) + 1
   END FUNCTION pos2cell
       
-  FUNCTION Verlet(Ex, Ey, init_pos, init_vel, dx, dy, dt, time)
+  SUBROUTINE Verlet(data_entry, Ex, Ey, init_pos, init_vel, dx, dy, dt, time)
     ! Input:
     !    Ex, Ey - the electic fields
     !    init_pos, init_vel - the initial positions and velocities (in a vector format)
@@ -38,7 +56,7 @@ CONTAINS
 !!!! verlet integrator and the gauss-seidel method.
 !!!! Right now, the grid sizes are being hard-coded into the function.
 !!!!    
-    TYPE(kinematics) :: Verlet
+    TYPE(kinematics) :: data_entry
     
     REAL(KIND=REAL64), INTENT(IN), DIMENSION(:,:) :: Ex, Ey ! field matrix
     REAL(KIND=REAL64), INTENT(IN), DIMENSION(2) :: init_pos ! initial position
@@ -52,18 +70,24 @@ CONTAINS
     
     INTEGER :: i, time, cellx, celly ! number of time-steps and loop variables
     INTEGER, DIMENSION(2) :: dims
-    dims = SHAPE(Ex)
    
     ! the initial data.
     pos_x = init_pos(1)
     pos_y = init_pos(2)
+    
     vel_x = init_vel(1)
     vel_y = init_vel(2)
     ! this bit uses the pos2cel function to access the electric fields.
-    acc_x = Ex(pos2cell(init_pos(1), dx), pos2cell(init_pos(2), dy))
-    acc_y = Ey(pos2cell(init_pos(1), dx), pos2cell(init_pos(2), dy)) 
+    acc_x = Ex(pos2cell(pos_x, dx), pos2cell(pos_y, dy))
+    acc_y = Ey(pos2cell(pos_x, dx), pos2cell(pos_y, dy)) 
 
     DO i = 1, time
+       data_entry%pos_history(1,i) = pos_x
+       data_entry%pos_history(2,i) = pos_y
+       data_entry%vel_history(1,i) = vel_x
+       data_entry%vel_history(2,i) = vel_y
+       data_entry%acc_history(1,i) = acc_x
+       data_entry%acc_history(2,i) = acc_y      
        
        ! store the accelerations
        old_acc_x = acc_x
@@ -77,7 +101,7 @@ CONTAINS
        celly = pos2cell(pos_y, dy)
        
        ! conditional doesn't allow us to pick values of the array that are larger than the array size.
-       IF ((cellx.GT.dims(1)-1).OR.(celly.GT.dims(2)-1)) THEN
+       IF ((cellx.GT.(-1)).OR.(celly.GT.(-1))) THEN
           acc_x = 0.0
           acc_y = 0.0
           vel_x = 0.0
@@ -89,12 +113,10 @@ CONTAINS
           ! make use of old_acc here
           vel_x = vel_x + dt*(0.5*(acc_x + old_acc_x))
           vel_y = vel_y + dt*(0.5*(acc_y + old_acc_y))
-       END IF
-       
-       print*, cellx, celly, pos_x, pos_y, vel_x, vel_y, acc_x, acc_y
+       END IF             
     END DO
-        
-  END FUNCTION Verlet
+    
+  END SUBROUTINE Verlet
   
 END MODULE verlet_integrator
 
@@ -102,6 +124,7 @@ END MODULE verlet_integrator
 PROGRAM main
   USE kinds
   USE verlet_integrator
+  
   IMPLICIT NONE
   
   REAL(KIND=REAL64), DIMENSION(10, 10) :: Ex, Ey
@@ -109,9 +132,9 @@ PROGRAM main
   REAL(KIND=REAL64), DIMENSION(2) :: init_vel
   REAL(KIND=REAL64) :: dx, dy, dt
   INTEGER :: time = 20
+  INTEGER :: i
+  TYPE(kinematics) :: test
   
-  TYPE(kinematics) :: test  
-
   Ex = 0.0
   Ey = 0.0
   init_pos(1) = 0.0
@@ -121,7 +144,25 @@ PROGRAM main
   dx = 0.1
   dy = 0.1
   dt = 0.01
+
+  CALL fillType(test, time)
+
+  ! REMOVING THIS BREAKS THE CODE
+  ! NOT SURE WHY
+  !------------------------------------------------------------------------------  
+  print*, shape(test%pos_history)
+  print*, shape(test%vel_history)
+  print*, shape(test%acc_history)
+  !------------------------------------------------------------------------------
+  CALL verlet(test, Ex, Ey, init_pos, init_vel, dx, dy, dt, time)
+
+  DO i = 1, time
+     print*, test%pos_history(1, i), test%pos_history(2, i)
+     print*, test%vel_history(1, i), test%vel_history(2, i)
+     print*, test%acc_history(1, i), test%acc_history(2, i)
+     print*, ""
+  END DO
   
-  test = verlet(Ex, Ey, init_pos, init_vel, dx, dy, dt, time)
+  CALL emptyType(test)
   
 END PROGRAM main
